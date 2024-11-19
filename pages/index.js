963765968c589c643f57a225d2732facb335b583
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2024-11-19 19:03:08 -0300

    Update index.js
    Test github 3.1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
 <<<<<<< HEAD
-    return <h1>Tes5</h1>
-=======
-    return <h1>Amor eu te amo, veja se apareceu a mensagem.</h1>
->>>>>>> d111c0e4eba5b66db4109f22b86e6be93de8d468
-}
+    return <h1>Test github 3.1</h1>
 
 export default Home;
