8294ae1ea1ab7611215c860f4888aea56401d2c3
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2024-11-19 19:01:19 -0300

    Test github 3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
+<<<<<<< HEAD
+    return <h1>Tes5</h1>
+=======
     return <h1>Amor eu te amo, veja se apareceu a mensagem.</h1>
+>>>>>>> d111c0e4eba5b66db4109f22b86e6be93de8d468
 }
 
 export default Home;
